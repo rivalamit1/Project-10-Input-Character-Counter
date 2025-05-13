44fedd844628bbd4e7a9a38e359e51494805d9d9
Amit Gupta <125515651+rivalamit1@users.noreply.github.com>
2025-05-13 22:23:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const count = document.querySelector("p");
 const input = document.querySelector("input");
-
+// on--///
+// input.addEventLIstener('keyup',()=>)
 input.addEventListener('keyup',()=>{
     count.innerHTML = input.value.length;
 });
